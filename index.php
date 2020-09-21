7d2af3baaf7be256699033a90c961937dcaee783
BukkitGerman <tanks110011@gmail.com>
2020-09-21 10:00:30 +0200

    Changes to be committed: 	new file:   css/navbar.css 	new file:   css/navbar.css.map 	new file:   css/navbar.sass 	new file:   css/style.css 	new file:   css/style.css.map 	new file:   css/style.sass 	new file:   images/logo.jpg 	new file:   images/logo.png 	new file:   images/logo.svg 	new file:   index.php 	new file:   templates/navbar.php
    Add Navbar and Navbar CSS for example.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  
+include "templates/navbar.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+<body>
+<?php 
+showNavbar();
+?>
+
+</body>
+</html>
