45396e6f7864994afecff20b0a40c604412ac270
BukkitGerman <tanks110011@gmail.com>
2020-09-28 12:40:56 +0200

    Edit Login CSS, renewed Login CSS

--- templates/login.php
+++ templates/login.php
@@ -2,8 +2,10 @@
 function showLogin(){
 	echo "
 	<div class='login'>
+		<div class='logo'><img class='logo' src='images/logo.png' alt='logo'></div>
 		<div class='title'>Login</div>
-		<div class='sub-title'>Administration</div>
+		<div class='sub-title'>Administraion</div>
+		<div class='username'
 	</div>
 	";	
 }
