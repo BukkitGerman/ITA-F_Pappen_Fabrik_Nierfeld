08df520c80d243f8b64f59541a333e46a9ea7bd3
BukkitGerman <tanks110011@gmail.com>
2020-09-28 12:13:05 +0200

    Change CSS

--- templates/login.php
+++ templates/login.php
@@ -1,13 +1,9 @@
 <?php
 function showLogin(){
 	echo "
-	<div class='outer-login'>
-		<form class='login' method='POST' action='/?p=admin'>
-			<h1>Login</h1>
-			<input type='text' name='username'/><br/>
-			<input type='password' name='password'/><br/>
-			<input type='submit' name='submit' value='Login'><br/>
-		</form>
+	<div class='login'>
+		<div class='title'>Login</div>
+		<div class='sub-title'>Administration</div>
 	</div>
 	";	
 }
