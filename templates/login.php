550cea4d1acf98b390ba536364ef382a39a3b5f5
BukkitGerman <tanks110011@gmail.com>
2020-09-28 10:13:45 +0200

    Change CSS, center Login Form in Page

--- templates/login.php
+++ templates/login.php
@@ -1,12 +1,14 @@
 <?php
 function showLogin(){
 	echo "
-	<form class='login' method='POST' action='/?p=admin'>
-		<h1>Login</h1>
-		<input type='text' name='username'/><br/>
-		<input type='password' name='password'/><br/>
-		<input type='submit' name='submit' value='Login'><br/>
-	</form>
+	<div class='outer-login'>
+		<form class='login' method='POST' action='/?p=admin'>
+			<h1>Login</h1>
+			<input type='text' name='username'/><br/>
+			<input type='password' name='password'/><br/>
+			<input type='submit' name='submit' value='Login'><br/>
+		</form>
+	</div>
 	";	
 }
 
